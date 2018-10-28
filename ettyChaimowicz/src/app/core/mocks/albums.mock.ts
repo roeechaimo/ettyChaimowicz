e20148eeace6ead5873097220867ab798397948d
roeechaimo <roee84@gmail.com>
2018-10-28 20:34:45 +0200

    change mock for album 1 and add todo

--- ettyChaimowicz/src/app/core/mocks/albums.mock.ts
+++ ettyChaimowicz/src/app/core/mocks/albums.mock.ts
@@ -1,40 +1,89 @@
 import { PaintingType } from "../models/painting.model";
 
+// TODO - add new links also to other albums and paintings
 export const ALBUMS = [
   {
     id: 1,
-    title: "Album number 1",
-    description: "1 blah blah",
+    title: "Optimistic",
+    description: "Optimistic album",
     paintings: [
       {
         id: 9,
-        name: "nine",
+        name: "optimistic_1",
         imageUrl:
-          "https://res.cloudinary.com/dwvfucbtk/image/upload/v1537785490/user_photos/ettyChaimowicz/gallery/1/paintings/black-background.jpg",
+          "https://res.cloudinary.com/dwvfucbtk/image/upload/v1540750918/user_photos/ettyChaimowicz/prod/gallery/1/optimistic/optimistic_1.jpg",
         type: PaintingType.ALBUM,
         externalId: 1
       },
       {
         id: 10,
-        name: "ten",
+        name: "optimistic_2",
         imageUrl:
-          "https://res.cloudinary.com/dwvfucbtk/image/upload/v1537785487/user_photos/ettyChaimowicz/gallery/1/paintings/apple.jpg",
+          "https://res.cloudinary.com/dwvfucbtk/image/upload/v1540750921/user_photos/ettyChaimowicz/prod/gallery/1/optimistic/optimistic_2.jpg",
         type: PaintingType.ALBUM,
         externalId: 1
       },
       {
         id: 11,
-        name: "eleven",
+        name: "optimistic_3",
         imageUrl:
-          "https://res.cloudinary.com/dwvfucbtk/image/upload/v1537785487/user_photos/ettyChaimowicz/gallery/1/paintings/burger.jpg",
+          "https://res.cloudinary.com/dwvfucbtk/image/upload/v1540750920/user_photos/ettyChaimowicz/prod/gallery/1/optimistic/optimistic_3.jpg",
         type: PaintingType.ALBUM,
         externalId: 1
       },
       {
         id: 12,
-        name: "four",
+        name: "optimistic_4",
         imageUrl:
-          "https://res.cloudinary.com/dwvfucbtk/image/upload/v1537785484/user_photos/ettyChaimowicz/gallery/1/paintings/back.jpg",
+          "https://res.cloudinary.com/dwvfucbtk/image/upload/v1540750920/user_photos/ettyChaimowicz/prod/gallery/1/optimistic/optimistic_4.jpg",
+        type: PaintingType.ALBUM,
+        externalId: 1
+      },
+      {
+        id: 13,
+        name: "optimistic_5",
+        imageUrl:
+          "https://res.cloudinary.com/dwvfucbtk/image/upload/v1540750921/user_photos/ettyChaimowicz/prod/gallery/1/optimistic/optimistic_5.jpg",
+        type: PaintingType.ALBUM,
+        externalId: 1
+      },
+      {
+        id: 14,
+        name: "optimistic_6",
+        imageUrl:
+          "https://res.cloudinary.com/dwvfucbtk/image/upload/v1540750920/user_photos/ettyChaimowicz/prod/gallery/1/optimistic/optimistic_6.jpg",
+        type: PaintingType.ALBUM,
+        externalId: 1
+      },
+      {
+        id: 15,
+        name: "optimistic_7",
+        imageUrl:
+          "https://res.cloudinary.com/dwvfucbtk/image/upload/v1540750918/user_photos/ettyChaimowicz/prod/gallery/1/optimistic/optimistic_7.jpg",
+        type: PaintingType.ALBUM,
+        externalId: 1
+      },
+      {
+        id: 16,
+        name: "optimistic_8",
+        imageUrl:
+          "https://res.cloudinary.com/dwvfucbtk/image/upload/v1540750923/user_photos/ettyChaimowicz/prod/gallery/1/optimistic/optimistic_8.jpg",
+        type: PaintingType.ALBUM,
+        externalId: 1
+      },
+      {
+        id: 17,
+        name: "optimistic_9",
+        imageUrl:
+          "https://res.cloudinary.com/dwvfucbtk/image/upload/v1540750923/user_photos/ettyChaimowicz/prod/gallery/1/optimistic/optimistic_9.jpg",
+        type: PaintingType.ALBUM,
+        externalId: 1
+      },
+      {
+        id: 18,
+        name: "optimistic_10",
+        imageUrl:
+          "https://res.cloudinary.com/dwvfucbtk/image/upload/v1540750922/user_photos/ettyChaimowicz/prod/gallery/1/optimistic/optimistic_10.jpg",
         type: PaintingType.ALBUM,
         externalId: 1
       }

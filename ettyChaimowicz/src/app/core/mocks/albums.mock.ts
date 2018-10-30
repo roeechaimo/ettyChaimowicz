811b3b8c3a2b51cc336602e48b51b112e895c5bc
roeechaimo <roee84@gmail.com>
2018-10-30 10:34:22 +0200

    add mobile album to albums mock

--- ettyChaimowicz/src/app/core/mocks/albums.mock.ts
+++ ettyChaimowicz/src/app/core/mocks/albums.mock.ts
@@ -134,5 +134,108 @@ export const ALBUMS = [
         externalId: 3
       }
     ]
+  },
+  {
+    id: 4,
+    title: "Mobile",
+    description: "Mobile album",
+    paintings: [
+      {
+        id: 23,
+        name: "mobile_1",
+        imageUrl:
+          "https://res.cloudinary.com/dwvfucbtk/image/upload/v1540887971/user_photos/ettyChaimowicz/prod/gallery/4/mobile/mobile_1.jpg",
+        type: PaintingType.ALBUM,
+        externalId: 4
+      },
+      {
+        id: 24,
+        name: "mobile_2",
+        imageUrl:
+          "https://res.cloudinary.com/dwvfucbtk/image/upload/v1540887971/user_photos/ettyChaimowicz/prod/gallery/4/mobile/mobile_2.jpg",
+        type: PaintingType.ALBUM,
+        externalId: 4
+      },
+      {
+        id: 25,
+        name: "mobile_3",
+        imageUrl:
+          "https://res.cloudinary.com/dwvfucbtk/image/upload/v1540887971/user_photos/ettyChaimowicz/prod/gallery/4/mobile/mobile_3.jpg",
+        type: PaintingType.ALBUM,
+        externalId: 4
+      },
+      {
+        id: 26,
+        name: "mobile_4",
+        imageUrl:
+          "https://res.cloudinary.com/dwvfucbtk/image/upload/v1540887975/user_photos/ettyChaimowicz/prod/gallery/4/mobile/mobile_4.jpg",
+        type: PaintingType.ALBUM,
+        externalId: 4
+      },
+      {
+        id: 27,
+        name: "mobile_5",
+        imageUrl:
+          "https://res.cloudinary.com/dwvfucbtk/image/upload/v1540887974/user_photos/ettyChaimowicz/prod/gallery/4/mobile/mobile_5.jpg",
+        type: PaintingType.ALBUM,
+        externalId: 4
+      },
+      {
+        id: 28,
+        name: "mobile_6",
+        imageUrl:
+          "https://res.cloudinary.com/dwvfucbtk/image/upload/v1540887974/user_photos/ettyChaimowicz/prod/gallery/4/mobile/mobile_6.jpg",
+        type: PaintingType.ALBUM,
+        externalId: 4
+      },
+      {
+        id: 29,
+        name: "mobile_7",
+        imageUrl:
+          "https://res.cloudinary.com/dwvfucbtk/image/upload/v1540887973/user_photos/ettyChaimowicz/prod/gallery/4/mobile/mobile_7.jpg",
+        type: PaintingType.ALBUM,
+        externalId: 4
+      },
+      {
+        id: 30,
+        name: "mobile_8",
+        imageUrl:
+          "https://res.cloudinary.com/dwvfucbtk/image/upload/v1540887974/user_photos/ettyChaimowicz/prod/gallery/4/mobile/mobile_8.jpg",
+        type: PaintingType.ALBUM,
+        externalId: 4
+      },
+      {
+        id: 31,
+        name: "mobile_9",
+        imageUrl:
+          "https://res.cloudinary.com/dwvfucbtk/image/upload/v1540887973/user_photos/ettyChaimowicz/prod/gallery/4/mobile/mobile_9.jpg",
+        type: PaintingType.ALBUM,
+        externalId: 4
+      },
+      {
+        id: 32,
+        name: "mobile_10",
+        imageUrl:
+          "https://res.cloudinary.com/dwvfucbtk/image/upload/v1540887973/user_photos/ettyChaimowicz/prod/gallery/4/mobile/mobile_10.jpg",
+        type: PaintingType.ALBUM,
+        externalId: 4
+      },
+      {
+        id: 33,
+        name: "mobile_11",
+        imageUrl:
+          "https://res.cloudinary.com/dwvfucbtk/image/upload/v1540887971/user_photos/ettyChaimowicz/prod/gallery/4/mobile/mobile_11.jpg",
+        type: PaintingType.ALBUM,
+        externalId: 4
+      },
+      {
+        id: 34,
+        name: "mobile_12",
+        imageUrl:
+          "https://res.cloudinary.com/dwvfucbtk/image/upload/v1540887971/user_photos/ettyChaimowicz/prod/gallery/4/mobile/mobile_12.jpg",
+        type: PaintingType.ALBUM,
+        externalId: 4
+      }
+    ]
   }
 ];
